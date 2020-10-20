ef6ae176b51d770b6d04c24c5bc3d8a8fef792dd
Parker Trussell <ptrussell1@cub.uca.edu>
2020-10-20 13:56:27 -0500

    Testing

---
+++ Project 2/project2package/PushCounter.java
@@ -0,0 +1,17 @@
+package project2package;
+
+import javax.swing.JFrame;
+
+public class PushCounter {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Push Counter");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		PushCounterPanel panel = new PushCounterPanel();
+		frame.getContentPane().add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
+
